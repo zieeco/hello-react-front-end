6aede308f33e706f3ad5f9a773bf83ab2a4ee9ad
zieeco <zieecosam@gmail.com>
2022-08-20 13:53:30 +0100

    Add some styling

--- src/App.js
+++ src/App.js
@@ -3,16 +3,25 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/configureStore';
+import logo from './logo.svg';
 import Greeting from './components/Greeting/greeting';
 
 const App = () => (
-  <Provider store={store}>
-    <BrowserRouter>
-      <Routes>
-        <Route exact path="/" element={<Greeting />} />
-      </Routes>
-    </BrowserRouter>
-  </Provider>
+  <div className="App">
+    <header className="App-header">
+      <div className="img-con">
+        <img src={logo} className="App-logo" alt="logo" />
+      </div>
+    </header>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Greeting />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  </div>
+
 );
 
 export default App;
